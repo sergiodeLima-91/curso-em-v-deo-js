a63ba622db497c86ab83262feefb8fee375cc01f
sergio-lima <sergiodelima22@gmail.com>
2024-01-31 09:31:41 -0300

    Primeiro Commit

---
+++ aula14/repeticao.js
@@ -0,0 +1,17 @@
+/* Estrutura de Repetição com Variável de Controle: HEXÁGONO REGULAR 
+
+1 - Inicialização
+2 - Teste Lógico
+3 - Incremento
+
+No momento do looping é feito o incremento (na volta)
+
+for (inicio; teste lógico; incremento){
+    (funcionalidade)
+}
+
+*/
+
+for (let Inicialização = 1; Inicialização <= 10; Inicialização++) {
+    console.log(`Passo ${Inicialização}`);
+}
