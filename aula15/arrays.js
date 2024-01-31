ba53dfaffee51b550ba6914dd56da4b7fa846541
sergio-lima <sergiodelima22@gmail.com>
2024-01-31 15:10:37 -0300

    Primeiro Commit

---
+++ aula15/arrays.js
@@ -0,0 +1,8 @@
+/* 
+
+Array (vetor ou variável composta) é uma variável com vários elementos compostos de chave (índice de identificação) e valor.  
+
+*/
+
+let numeros = [1,2,3,'B',5];
+console.log(`O vetor é ${numeros}`)
