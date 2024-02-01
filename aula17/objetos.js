74908e13528ad0caed625b1f91b93ef917ff5bae
sergio-lima <sergiodelima22@gmail.com>
2024-02-01 15:16:52 -0300

    Finalizacao do Curso

---
+++ aula17/objetos.js
@@ -0,0 +1,14 @@
+let objeto = {
+    nome: 'Sérgio',
+    idade: 33,
+    sexo: 'M',
+
+    falar() {
+        return 'Olá, como vai?'
+    }
+};
+
+console.log(objeto.nome)
+console.log(objeto.idade)
+console.log(objeto.sexo)
+console.log(objeto.falar())
