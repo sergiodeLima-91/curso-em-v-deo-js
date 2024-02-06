f3878cb31861af78d273b94684559b2642ced5bc
sergio-lima <sergiodelima22@gmail.com>
2024-02-06 15:37:52 -0300

    Trabalhando exibicao dos resultados depois de pressionar o botao

---
+++ exercicios/verificadordeidade/src/scripts/main.js
@@ -0,0 +1,7 @@
+var texto = document.getElementById("results");
+var button = document.getElementById("button");
+var image = document.getAnimations("image");
+
+button.addEventListener("click", function() {
+    button.classList.add("mostrar");
+})
