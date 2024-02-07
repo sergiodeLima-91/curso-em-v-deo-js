89555e03c56851eedab03733d86706f880290b78
sergio-lima <sergiodelima22@gmail.com>
2024-02-07 15:39:50 -0300

    Calculo da Idade

--- exercicios/verificadordeidade/src/scripts/main.js
+++ exercicios/verificadordeidade/src/scripts/main.js
@@ -1,8 +1,18 @@
 var results = document.getElementById("results");
 var button = document.getElementById("button");
-var teste = document.getElementById("img");
+var image = document.getElementById("img");
+var description = document.getElementById("desc");
+var year = document.getElementById("age-input");
+// Convertendo tipo do input de string para número:
+var year_num = Number(year.value);
+// Criando variável para pegar ano atual pelo computador:
+var now = new Date();
+var currentYear = now.getFullYear();
 
+
+// Acionando o resultado pelo evento de clique po escuta:
 button.addEventListener("click", function() {
     results.classList.add("hide-results");
-    teste.classList.remove("image");
+    image.classList.remove("image");
+    description.innerHTML = `Idade ${currentYear - year.value}`;
 });
