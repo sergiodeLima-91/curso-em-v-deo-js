6081396672f1cf95bef2a7d4618240b0bf8d3ef1
sergio-lima <sergiodelima22@gmail.com>
2024-02-21 10:29:56 -0300

    Refatoracao do HTML

--- exercicios/verificadordeidade/src/scripts/main.js
+++ exercicios/verificadordeidade/src/scripts/main.js
@@ -1,39 +1,11 @@
-var results = document.getElementById("results");
+// Seleção de Elementos via DOM:
 var button = document.getElementById("button");
+var results = document.getElementById("results");
 var image = document.getElementById("img");
-var description = document.getElementById("desc");
-var year = document.getElementById("age-input");
-// Convertendo tipo do input de string para número:
-var year_num = Number(year.value);
-// Criando variável para pegar ano atual pelo computador:
-var now = new Date();
-var currentYear = now.getFullYear();
-var sexo = '';
-var idade = '';
 
-// Acionando o resultado pelo evento de clique po escuta:
-button.addEventListener("click", function showResults(){
+// Evento de clique:
+button.addEventListener("click", function() {
     results.classList.add("hide-results");
     image.classList.remove("image");
-    description.innerHTML = `Sexo: ${sexDetection()} / Idade: ${ageCalculation()}`;
-});
 
-function sexDetection() {
-    return sexo = document.querySelector('input[name="sexo"]:checked').value;
-}
-
-function ageCalculation() {
-    return idade = currentYear - year.value;
-}
-
-// Validação dos dados:
-var formYear = document.getElementById("form-year");
-formYear.addEventListener("submit", function noSendForm(event) {
-    // Verifica se o campo
-    if (!formYear.checkValidity()) {
-        alert("Por favor, preencha os campos obrigatórios!")
-        event.preventDefault(); // Impede o envio do formulário
-    }else{
-        alert("Dados enviados!")
-    }
-});
+})
